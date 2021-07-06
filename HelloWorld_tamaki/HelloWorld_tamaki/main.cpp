5c59342dba39bfc434bd009b550ee10b29d81528
n_sato <n.satou@monochrome-corp.com>
2021-07-06 16:12:32 +0900

    main関数で自己紹介するプログラムを追加

--- HelloWorld_tamaki/HelloWorld_tamaki/main.cpp
+++ HelloWorld_tamaki/HelloWorld_tamaki/main.cpp
@@ -1,10 +1,13 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
+#include "sato.h"
 
 int main()
 {
 
 	printf("HelloWorld\n");
+	Sato my;
+	my.ShowName();
 
 	system("pause");
 	return 0;
