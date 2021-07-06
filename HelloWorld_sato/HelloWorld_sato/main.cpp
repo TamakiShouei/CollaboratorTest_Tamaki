a1eae6a449450bf8b4fa28656ccac3d855d23ad1
LAPTOP-P55ULRO3\humankyoto-04 <t.shouei20040730@gmail.com>
2021-07-06 16:40:45 +0900

    HelloWorld_satoにメイン処理の追加

--- HelloWorld_sato/HelloWorld_sato/main.cpp
+++ HelloWorld_sato/HelloWorld_sato/main.cpp
@@ -1,10 +1,13 @@
 ﻿
 #include <stdio.h>
 #include <stdlib.h>
+#include "sato.h"
 
 int main( void )
 {
 	printf("HelloWorld\n");
+	Sato myTamaki;
+	myTamaki.ShowName();
 
 	system("pause");
 	return 0;
